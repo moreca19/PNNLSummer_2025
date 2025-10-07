edeed2bab8c6436cdd1c89df4904491eb0250206
Your Name <you@example.com>
2025-10-07 13:29:34 -0700

    added new file

---
+++ python/MyNewGlobals.py
@@ -0,0 +1,23 @@
+from gegede import Quantity as Q
+
+
+fht = Q('841.1cm')
+fst = Q('896.4cm')
+fSpacing = Q('157.86cm')
+fIFlangeWidth = Q('40.2cm')
+fIFlangeThick = Q('4cm')
+fIFlangeWaist = Q('2.2cm')
+fIFlangeHeight = Q('110.8cm')
+fIPortSpacing = Q('400cm')
+fITopLength = Q('1783.2cm') + fIFlangeHeight
+fISideLength = Q('1473.2cm') + fIFlangeHeight
+fIPortHoleRad = Q('40cm')	
+fColdSkinThickness = Q('0.18cm')
+Offset = Q('10cm')
+fCryostat_x = Q('1420cm')
+fCryostat_y = Q('1390cm')
+fCryostat_z = Q('6000cm')
+fShieldThickness = Q('77.6cm')
+fWoodThickness = Q('4.8cm')
+fWarmSkinThickness = Q('2.4cm')
+BlockThickness = Q('0.40m')
